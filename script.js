1a32510d4d39aefbbb25b73899fa4953642eef0b
Anuj3558 <anujloharkar3557@gmail.com>
2024-03-01 08:40:14 +0530

    animation added

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const observer = new IntersectionObserver((entriers) = >{
-        entries.forEach(entry) = >{
+
+const observer = new IntersectionObserver((entriers) =>{
+        entriers.forEach((entry) =>{
                 console.log(entry)
-                if(entry.isIntersecting){
+                if(entry.isIntersecting ){
                              entry.target.classList.add('show');
+                             
                 }
-                else{
-                          enrty.traget.classList.remove('show');
-                }
+                
+                
         });
+        
 });
 const hiddenElemnts = document.querySelectorAll('.hidden');
-hiddenElemnts.forEach((el)=>observer.observer(el));
+hiddenElemnts.forEach((el)=>observer.observe(el));
